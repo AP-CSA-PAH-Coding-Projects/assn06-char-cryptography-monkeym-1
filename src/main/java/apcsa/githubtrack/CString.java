91bb81aa8faa59b4cf9ca25ea1e4831f650d923a
monkeym-1 <marc317j@gmail.com>
2026-04-03 20:36:15 +0300

    Add CString class with string manipulation methods
    Implemented CString class with methods for reversing, sorting in ascending and descending order, and converting to string.

--- src/main/java/apcsa/githubtrack/CString.java
+++ src/main/java/apcsa/githubtrack/CString.java
@@ -1,3 +1,63 @@
 package apcsa.githubtrack;
 
-// Implement your CString class here
+public class CString {
+    private char[] data;
+     // Constructs a CString from a Java String.
+     // Does not store the String object, only the char array.
+     
+    public CString(String s) {
+        if (s == null) {
+            this.data = new char[0];
+        } else {
+            this.data = s.toCharArray();
+        }
+    }
+    public void reverse() {
+        int left = 0;
+        int right = data.length - 1;
+        while (left < right) {
+            char temp = data[left];
+            data[left] = data[right];
+            data[right] = temp;
+            left++;
+            right--;
+        }
+    }
+    // Sorts characters in ascending order using Selection Sort.
+    public void sortAscending() {
+        for (int i = 0; i < data.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < data.length; j++) {
+                if (data[j] < data[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            char temp = data[minIdx];
+            data[minIdx] = data[i];
+            data[i] = temp;
+        }
+    }
+    // Sorts characters in descending order using Insertion Sort.
+    public void sortDescending() {
+        for (int i = 1; i < data.length; i++) {
+            char key = data[i];
+            int j = i - 1;
+            // Move elements greater than key to one position ahead
+            while (j >= 0 && data[j] < key) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = key;
+        }
+    }
+    public char[] getData() {
+        return data;
+    }
+    public int length() {
+        return data.length;
+    }
+    // Returns the CString.
+    public String toString() {
+        return new String(data);
+    }
+}
